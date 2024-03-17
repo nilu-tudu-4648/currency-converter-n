3f0c76d6b783233ae7ebfe6c5e64f5b33971aa3e
tudunilesh3@gmail.com <nilesh.tudu@nirmitee.io>
2024-03-17 16:28:00 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_qTHpjb02DvhS5maEIGoVhusDe4bZpL8kl1i1nuGw');
 
-
+//first convertcurrency
 export async function convertCurrency(fromCurrency,toCurrency,units) {
  const res = await  freecurrencyapi.latest({
         base_currency: fromCurrency,
